aca3bf661c99b04391369357c8b65b835e124024
prachi23gupta <prachi2022gupta@gmail.com>
2026-03-29 18:05:50 +0530

    Delete utils directory

--- utils/parser.py
+++
@@ -1,9 +0,0 @@
-import PyPDF2
-
-def extract_text_from_pdf(file):
-    reader = PyPDF2.PdfReader(file)
-    text = ""
-    for page in reader.pages:
-        if page.extract_text():
-            text += page.extract_text()
-    return text.lower()
